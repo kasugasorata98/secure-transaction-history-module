8a4e623fabe5668dd98a5d859963141595306360
jeromykho <jeromy@acsdigital.com.my>
2024-08-24 10:45:14 +0800

    chore: design improvement

--- app/transaction-details.tsx
+++ app/transaction-details.tsx
@@ -24,7 +24,7 @@ export default function TransactionDetailsScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.name}>{transaction.name}</Text>
-      <View style={styles.body}>
+      <View style={styles.switchContainer}>
         <Switch
           onValueChange={onSwitch}
           value={shouldRevealAmount}
@@ -32,27 +32,29 @@ export default function TransactionDetailsScreen() {
         />
         <Text>Reveal Amount</Text>
       </View>
-      <View style={styles.data}>
-        <Text>Amount</Text>
-        <Text>
-          RM {shouldRevealAmount ? transaction.amount.toFixed(2) : "****.**"}
-        </Text>
-      </View>
-      <View style={styles.data}>
-        <Text>Date</Text>
-        <Text>{transaction.date.toDateString()}</Text>
-      </View>
-      <View>
-        <Text>Description</Text>
-        <Text>{transaction.description}</Text>
-      </View>
-      <View style={styles.data}>
-        <Text>Ref. Number</Text>
-        <Text>{transaction.referenceNumber}</Text>
-      </View>
-      <View style={styles.data}>
-        <Text>Transaction Type</Text>
-        <Text>{transaction.type === "debit" ? "Debit" : "Credit"}</Text>
+      <View style={styles.body}>
+        <View style={styles.data}>
+          <Text>Amount</Text>
+          <Text>
+            RM {shouldRevealAmount ? transaction.amount.toFixed(2) : "****.**"}
+          </Text>
+        </View>
+        <View style={styles.data}>
+          <Text>Date</Text>
+          <Text>{transaction.date.toDateString()}</Text>
+        </View>
+        <View>
+          <Text style={styles.description}>Description</Text>
+          <Text>{transaction.description}</Text>
+        </View>
+        <View style={styles.data}>
+          <Text>Ref. Number</Text>
+          <Text>{transaction.referenceNumber}</Text>
+        </View>
+        <View style={styles.data}>
+          <Text>Transaction Type</Text>
+          <Text>{transaction.type === "debit" ? "Debit" : "Credit"}</Text>
+        </View>
       </View>
     </ScrollView>
   );
@@ -63,14 +65,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   name: {
+    fontSize: 18,
     textAlign: "center",
-    marginVertical: 15,
+    marginTop: 15,
+    marginBottom: 10,
   },
   body: {
+    gap: 25,
+  },
+  switchContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     gap: 5,
+    marginBottom: 10,
   },
   data: {
     flexDirection: "row",
@@ -79,4 +87,5 @@ const styles = StyleSheet.create({
   switch: {
     transform: [{ scale: 0.7 }],
   },
+  description: { textDecorationLine: "underline" },
 });
